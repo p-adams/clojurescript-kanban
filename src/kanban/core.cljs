f83f9ac2e81a5eb68c40f3ae7cad18b68df6a3bf
User <paadams@nmu.edu>
2017-06-22 07:47:59 -0400

    fixed move functionality

--- src/kanban/core.cljs
+++ src/kanban/core.cljs
@@ -1,7 +1,7 @@
 (ns kanban.core
     (:require [reagent.core :as reagent]))
 
-(defonce backlog-state (reagent/atom {1 {:id 1 :task "create RESTFUL endpoints" :completed false :owner "Mary Smitgh"}
+(defonce backlog-state (reagent/atom {1 {:id 1 :task "create RESTFUL endpoints" :completed false :owner "Mary Smith"}
                                   2 {:id 2 :task "design UI prototype" :completed false :owner "Safdar Khan"}}))
 (defonce dev-state (reagent/atom {3 {:id 3 :task "client protocol" :completed false :owner "Mark Wu"}}))
 (defonce test-state (reagent/atom {4 {:id 4 :task "server protocol" :completed false :owner "Maryam Patel"}}))
@@ -21,7 +21,12 @@
 
 (defn move-activity [activity activities destination]
   "move an activity to a new destination"
-  (js/alert (for [dest destination] dest)))
+  (let [id (get (val activity) :id)
+        task (get (val activity) :task)
+        completed (get (val activity) :completed)
+        owner (get (val activity) :owner)]
+  (swap! destination assoc id {:id id :task task :completed completed :owner owner}))
+  (remove-activity (get (val activity) :id) activities))
 
 (defn render-activities [activities button-text destination]
   "iterate through activities; render each activity as list item"
@@ -31,13 +36,11 @@
     ^{:key activity}
     [:li "Task: "(get (val activity) :task) [:br] " Owner: "(get (val activity) :owner)
      [:br]
-     [:button {:on-click #(remove-activity (get (val activity) :id) activities)
-                ;:on-click #(move-activity (get activity :task) activities destination)
-                }
+     [:button {:on-click #(move-activity activity activities destination)}
                 button-text]])])
 
 (defn add-task-to-backlog [task owner]
-  "add new task to backlog and reset the inputs"
+  "adds new task to backlog and resets the inputs"
   (let [id (swap! id-counter inc)]
     (swap! backlog-state assoc id {:id id :task task :completed false :owner owner}))
   (reset-inputs))
